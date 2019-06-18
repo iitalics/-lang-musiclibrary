d833ddcb0eb16096fe6c86c45c43e966bc130a23
Milo Turner <iitalics@gmail.com>
2019-06-18 04:42:51 -0400

    Simple process-track

--- main.rkt
+++ main.rkt
@@ -7,6 +7,7 @@
 
 ;; ---------------------------------------------------------------------------------------
 ;; FFmpeg
+;; --------------------
 
 ;; (current-ffmpeg) : path
 (define current-ffmpeg
@@ -33,15 +34,88 @@
   (define FFMPEG-VERSION-REGEX
     #px"^ffmpeg version .* Copyright \\(c\\) .* the FFmpeg developers")
 
-  (let-values ([(sc stdout stderr) (exec-ffmpeg '("-version"))])
-    (check-equal? sc 0)
+  (let-values ([(code stdout stderr) (exec-ffmpeg '("-version"))])
+    (check-equal? code 0)
     (check-pred (curry regexp-match? FFMPEG-VERSION-REGEX)
                 (port->string stdout))
     (check-equal? (port->string stderr) "")))
 
 ;; ---------------------------------------------------------------------------------------
 ;; Processing tracks
+;; --------------------
 
 ;; (current-output-directory) : path
 (define current-output-directory
   (make-parameter (build-path "./musiclibrary")))
+
+;; (process-track trk) : void
+;; trk : track
+(define (process-track trk)
+  (define-values [code _stdout _stderr]
+    (exec-ffmpeg `("-y"
+                   "-i"
+                   ,(track-audio-src trk)
+                   ,(build-path (current-output-directory)
+                                (track-output-path trk)))))
+  (unless (zero? code)
+    (error 'process-track
+           (format "ffmpeg returned status code: ~a" code))))
+
+;; =======================================================================================
+
+(module+ test
+
+  ;; -----------------------------------
+  ;; Utils
+
+  ;; (recursively-delete-directory d) : void
+  ;; d : path-string
+  (define (recursively-delete-directory d)
+    (define leftover-dirs
+      (for/fold ([l (list d)])
+                ([p (in-directory d)])
+        (if (directory-exists? p)
+          (cons p l)
+          (begin (delete-file p)
+                 l))))
+    (for-each delete-directory leftover-dirs))
+
+  ;; [-> A] -> A
+  (define (call/test-musiclibrary f)
+    (parameterize ([current-output-directory
+                    (build-path "./test-musiclibrary")])
+      (define (del)
+        (when (directory-exists? (current-output-directory))
+          (recursively-delete-directory (current-output-directory))))
+
+      (del)
+      (make-directory (current-output-directory))
+      (define result
+        (with-handlers ([exn?
+                         (λ (e)
+                           (del)
+                           (raise e))])
+          (f)))
+      (del)
+      result))
+
+  (define-syntax-rule (with-test-musiclibrary body ...)
+    (call/test-musiclibrary (λ () body ...)))
+
+  ;; -----------------------------------
+
+  (define test-audio-path
+    (build-path (current-directory)
+                "test-audio.ogg"))
+
+  (check-pred file-exists? test-audio-path
+              "example audio file (test-audio.ogg) must exist")
+
+  (with-test-musiclibrary
+    ; TODO: check metadata?? how??
+    (process-track (track #:audio-src test-audio-path
+                          #:output-path "test-audio.mp3"
+                          'title "Foo"))
+    (check-equal?
+     (directory-list (current-output-directory))
+     (list (build-path "test-audio.mp3")))))
